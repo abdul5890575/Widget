4b41b74b3d0f8454a48ca900dc29a3d10baf1019
Abdul Rehman Shaukat <abdulrehman.shoukat@gmail.com>
2020-11-16 20:26:42 -0500

    Updates

---
+++ precache-manifest.00e60260df34674b2d85db5530feb6a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18eff42245f42ffdfa2028fc1d1edba",
+    "url": "/Widget/index.html"
+  },
+  {
+    "revision": "1c99b579491d2147ba1f",
+    "url": "/Widget/static/js/2.6f6cdba4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Widget/static/js/2.6f6cdba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a2a3216f97a8541048",
+    "url": "/Widget/static/js/main.77296939.chunk.js"
+  },
+  {
+    "revision": "171126ac4f23793ba0fe",
+    "url": "/Widget/static/js/runtime-main.a7183905.js"
+  }
+]);
